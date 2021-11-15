e2f198879450c0188bc52fccd2ea303940a730d8
mrssmith <marisolbsmith@yahoo.com>
2021-11-14 20:54:33 -0600

    boiler plate

---
+++ controllers/home-routes.js
@@ -0,0 +1,101 @@
+const router = require('express').Router();
+const { User, Blog } = require('../models');
+const withAuth = require('../utils/auth');
+
+//render sign up page
+router.get("/signup"), async (req, res) => {
+  try {
+    res.render('signup');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+}; 
+  
+  /*router.get('/', async (req, res) => {
+  try {
+    const dbGalleryData = await User.findAll({
+      include: [
+        {
+          model: Blog,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
+
+    const galleries = dbGalleryData.map((gallery) =>
+      gallery.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/gallery/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the gallery
+    try {
+      const dbGalleryData = await User.findByPk(req.params.id, {
+        include: [
+          {
+            model: Blog,
+            attributes: [
+              'id',
+              'title',
+              'artist',
+              'exhibition_date',
+              'filename',
+              'description',
+            ],
+          },
+        ],
+      });
+      const gallery = dbGalleryData.get({ plain: true });
+      res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  // If the user is not logged in, redirect the user to the login page
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, allow them to view the painting
+    try {
+      const dbPaintingData = await Blog.findByPk(req.params.id);
+
+      const painting = dbPaintingData.get({ plain: true });
+
+      res.render('painting', { painting, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
