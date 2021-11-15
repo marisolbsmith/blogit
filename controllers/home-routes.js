f262cd124427ac057700faf00da9737428239fef
mrssmith <marisolbsmith@yahoo.com>
2021-11-14 23:16:26 -0600

    debugging

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,101 +1,103 @@
-const router = require('express').Router();
-const { User, Blog } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Blog, } = require("../models");
+const withAuth = require("../utils/auth");
 
-//render sign up page
-router.get("/signup"), async (req, res) => {
-  try {
-    res.render('signup');
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-}; 
-  
-  /*router.get('/', async (req, res) => {
-  try {
-    const dbGalleryData = await User.findAll({
-      include: [
-        {
-          model: Blog,
-          attributes: ['filename', 'description'],
-        },
-      ],
-    });
-
-    const galleries = dbGalleryData.map((gallery) =>
-      gallery.get({ plain: true })
-    );
-
-    res.render('homepage', {
-      galleries,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+// Homepage
+router.get("/", withAuth, async (req, res) => {
+try {
+// Link post with user
+const bpData = await Post.findAll({
+include: [{ model: User }],
 });
 
-// GET one gallery
-router.get('/gallery/:id', async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  } else {
-    // If the user is logged in, allow them to view the gallery
-    try {
-      const dbGalleryData = await User.findByPk(req.params.id, {
-        include: [
-          {
-            model: Blog,
-            attributes: [
-              'id',
-              'title',
-              'artist',
-              'exhibition_date',
-              'filename',
-              'description',
-            ],
-          },
-        ],
-      });
-      const gallery = dbGalleryData.get({ plain: true });
-      res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  }
+const bp = bpData.map((post) => post.get({ plain: true }));
+console.log(bp);
+
+res.render("home", { bp });
+} catch (err) {
+res.status(500).json(err);
+}
 });
 
-// GET one painting
-router.get('/painting/:id', async (req, res) => {
-  // If the user is not logged in, redirect the user to the login page
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  } else {
-    // If the user is logged in, allow them to view the painting
-    try {
-      const dbPaintingData = await Blog.findByPk(req.params.id);
 
-      const painting = dbPaintingData.get({ plain: true });
 
-      res.render('painting', { painting, loggedIn: req.session.loggedIn });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  }
+// Render login page
+router.get("/login", (req, res) => {
+try {
+res.render("login");
+} catch (err) {
+res.status(500).json(err);
+}
 });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('login');
+// Render sign up page
+router.get("/signup", async (req, res) => {
+try {
+res.render("signup");
+} catch (err) {
+res.status(500).json(err);
+}
 });
+
+// Render New Post page
+router.get("/newPost", (req, res) => {
+try {
+res.render("newPost");
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
+// render editPost page
+router.get("/editPost/:id", async (req, res) => {
+try {
+const bpData = await Post.findByPk(req.params.id, {
+include: [{ model: User }],
+});
+const bp = bpData.get({ plain: true });
+
+res.render("editPost", { bp });
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
+// Get user by id linked to comments
+router.get("/user/all/:id", async (req, res) => {
+try {
+const userData = await User.findByPk(req.params.id, {
+include: [{ model: Post }, { model: Comment }],
+});
+
+if (!userData) {
+res.status(404).json({ message: "No users found with this id" });
+} else {
+res.status(200).json(userData);
+}
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
+// Get post by id linked to user
+router.get("/post/all/:id", async (req, res) => {
+try {
+const bpData = await Post.findByPk(req.params.id, {
+include: [{ model: User }, { model: Comment }],
+});
+
+if (!bpData) {
+res.status(404).json({ message: "No blog posts found with this id" });
+} else {
+res.status(200).json(bpData);
+}
+} catch (err) {
+res.status(500).json(err);
+}
+});
+
 
 module.exports = router;
+
+  
+  
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
 DROP DATABASE IF EXISTS blogit_db;
 
 CREATE DATABASE blogit_db;
+
+DB_NAME=blogit_db
+DB_USER="root"
+DB_PASSWORD="Bianca0110"
